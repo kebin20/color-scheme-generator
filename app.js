5002db9010a611e1e7b2c8d6ac53752ec6ae3e9b
kebin20 <ktanzyl@hotmail.com>
2022-10-16 14:57:23 +0900

    attempted to refactor, but failed

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // HTML Components
-const colorStrip1 = document.getElementById("color-strip-one");
-const colorStrip2 = document.getElementById("color-strip-two");
-const colorStrip3 = document.getElementById("color-strip-three");
-const colorStrip4 = document.getElementById("color-strip-four");
-const colorStrip5 = document.getElementById("color-strip-five");
-const hexNumberDisplay1 = document.getElementById("hex-number-one");
-const hexNumberDisplay2 = document.getElementById("hex-number-two");
-const hexNumberDisplay3 = document.getElementById("hex-number-three");
-const hexNumberDisplay4 = document.getElementById("hex-number-four");
-const hexNumberDisplay5 = document.getElementById("hex-number-five");
+// const colorStrip1 = document.getElementById("color-strip-1");
+// const colorStrip2 = document.getElementById("color-strip-2");
+// const colorStrip3 = document.getElementById("color-strip-3");
+// const colorStrip4 = document.getElementById("color-strip-4");
+// const colorStrip5 = document.getElementById("color-strip-5");
+
+const numbers = [1, 2, 3, 4, 5];
+
+const colorStrips = numbers.map((number) =>
+  document.getElementById(`color-strip-${number}`)
+);
+
+const hexNumberDisplays = numbers.map((number) =>
+  document.getElementById(`hex-number-${number}`)
+);
+
+// const hexNumberDisplay1 = document.getElementById("hex-number-1");
+// const hexNumberDisplay2 = document.getElementById("hex-number-2");
+// const hexNumberDisplay3 = document.getElementById("hex-number-3");
+// const hexNumberDisplay4 = document.getElementById("hex-number-4");
+// const hexNumberDisplay5 = document.getElementById("hex-number-5");
 const getColorBtn = document.getElementById("get-color-scheme");
 const scheme = document.getElementById("scheme-select");
 
@@ -29,23 +40,34 @@ function fetchColor() {
   )
     .then((res) => res.json())
     .then((data) => {
-      const colorData1 = data.colors[0].hex.value;
-      const colorData2 = data.colors[1].hex.value;
-      const colorData3 = data.colors[2].hex.value;
-      const colorData4 = data.colors[3].hex.value;
-      const colorData5 = data.colors[4].hex.value;
+      let hexNumberData = data.colors;
+      // const colorData1 = data.colors[0].hex.value;
+      // const colorData2 = data.colors[1].hex.value;
+      // const colorData3 = data.colors[2].hex.value;
+      // const colorData4 = data.colors[3].hex.value;
+      // const colorData5 = data.colors[4].hex.value;
 
-      colorStrip1.style.backgroundColor = colorData1;
-      colorStrip2.style.backgroundColor = colorData2;
-      colorStrip3.style.backgroundColor = colorData3;
-      colorStrip4.style.backgroundColor = colorData4;
-      colorStrip5.style.backgroundColor = colorData5;
+      // colorStrip1.style.backgroundColor = colorData1;
+      // colorStrip2.style.backgroundColor = colorData2;
+      // colorStrip3.style.backgroundColor = colorData3;
+      // colorStrip4.style.backgroundColor = colorData4;
+      // colorStrip5.style.backgroundColor = colorData5;
 
-      hexNumberDisplay1.textContent = colorData1;
-      hexNumberDisplay2.textContent = colorData2;
-      hexNumberDisplay3.textContent = colorData3;
-      hexNumberDisplay4.textContent = colorData4;
-      hexNumberDisplay5.textContent = colorData5;
+      // hexNumberDisplay1.textContent = colorData1;
+      // hexNumberDisplay2.textContent = colorData2;
+      // hexNumberDisplay3.textContent = colorData3;
+      // hexNumberDisplay4.textContent = colorData4;
+      // hexNumberDisplay5.textContent = colorData5;
+
+      const colorData = hexNumberData.forEach((number) =>
+        console.log(number.hex.value)
+      );
+
+      colorStrips.forEach((strip) => {
+        strip.style.backgroundColor = colorData;
+      });
+
+      // hexNumberDisplays.textContent = colorData;
 
       console.log(data);
     });
